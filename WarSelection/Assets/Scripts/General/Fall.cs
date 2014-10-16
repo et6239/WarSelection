c93353bd0e8ad5150051b9d07a0ef3a490acb3e8
Michael Vaganov <mvaganov@hotmail.com>
2014-10-15 18:38:27 -0700

    added fall script to general scripts folder

---
+++ WarSelection/Assets/Scripts/General/Fall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	Vector3 startLocation;
+
+	public float minimumY = -10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		startLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(transform.position.y < minimumY)
+		{
+			transform.position = startLocation;
+		}
+	}
+}
