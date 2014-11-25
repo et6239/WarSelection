7288ab326536adb060353bf6067a8d559588baa3
Michael Vaganov <mvaganov@hotmail.com>
2014-11-24 18:06:28 -0800

    michael's level update
    * texture stuff
    * added cave
    * added nextLevel script

---
+++ WarSelection/Assets/Scripts/Michael/NextZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (SphereCollider))]
+public class NextZone : MonoBehaviour {
+
+	public string nextZone;
+
+	void OnTriggerEnter(Collider other) {
+		if(nextZone != null && nextZone.Length > 0
+		  && other.gameObject.GetComponent<CharacterController>()) {
+			Application.LoadLevel(nextZone);
+		}
+	}
+}
