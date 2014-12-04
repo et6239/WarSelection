3e36b900a7ba02d68cc4f02d57e5558be1363748
Michael Vaganov <mvaganov@hotmail.com>
2014-12-03 18:37:18 -0800

    player stats

---
+++ WarSelection/Assets/Scripts/Yuval/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+	/// health
+	public float hp;
+	/// for sprinting and attacks and jumps (all physical activity)
+	public float stamina;
+	/// magic points
+	public float mp;
+	/// attack score
+	public float att;
+	/// defense score
+	public float def;
+	/// dexterity
+	public float dex;
+	/// wisdom
+	public float wis;
+	/// vitality
+	public float vit;
+	/// speed
+	public float spd;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
