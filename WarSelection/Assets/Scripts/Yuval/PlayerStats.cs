6589a2d24a36926f2ed36a8e5a59fc39cd847ef1
Michael Vaganov <mvaganov@hotmail.com>
2014-12-10 18:32:02 -0800

    hp bar
    press F and G to change the HP

--- WarSelection/Assets/Scripts/Yuval/PlayerStats.cs
+++ WarSelection/Assets/Scripts/Yuval/PlayerStats.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
 	/// health
-	public float hp = 7, maxhp = 10;
+	public float hp = 10, maxhp = 10;
+
+	public UnityEngine.UI.Image hpBar;
+	public UnityEngine.UI.Text hpText;
+	float healthBarStart;
+
 	/// for sprinting and attacks and jumps (all physical activity)
 	public float stamina;
 	/// magic points
@@ -23,11 +29,24 @@ public class PlayerStats : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		healthBarStart = 0;// hpBar.rectTransform.anchoredPosition.x;
 	}
-	
+
+	void addHP(int hpAdjustment) {
+		hp += hpAdjustment;
+		hpText.text = "Current HP: " + hp;
+		Vector2 pos = hpBar.rectTransform.anchoredPosition;
+		pos.x = hp - maxhp;//hp + healthBarStart - maxhp;
+		hpBar.rectTransform.anchoredPosition = pos;
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.F)) {
+			addHP(-1);
+		}
+		if(Input.GetKeyDown(KeyCode.G)) {
+			addHP(+1);
+		}
 	}
 }
