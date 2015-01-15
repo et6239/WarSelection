11606afc8c5b0a0f980e04a159af369fe78876c4
Michael Vaganov <mvaganov@hotmail.com>
2015-01-14 18:25:40 -0800

    forgot to commit stuff

---
+++ WarSelection/Assets/Scripts/Michael/PlayerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public Transform cubePrefab;
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+	}
+
+	void OnConnectedToServer()
+	{
+		SpawnPlayer();
+	}
+
+	void SpawnPlayer()
+	{
+		Transform myTransform = (Transform)Network.Instantiate(
+			cubePrefab, transform.position, transform.rotation, 0);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ WarSelection/Assets/Scripts/Michael/Spawner2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// TODO finish from Non-Authoratitive/Authoratitive Server
+
+public class Spawner2 : MonoBehaviour {
+
+	public Transform playerPrefab;
+	public ArrayList playerScripts = new ArrayList();
+
+	void OnServerInitialized()
+	{
+		SpawnPlayer(Network.player);
+	}
+
+	void OnPlayerConnected(NetworkPlayer player)
+	{
+		SpawnPlayer(player);
+	}
+
+	void SpawnPlayer(NetworkPlayer player)
+	{
+		string tempPlayerString = player.ToString();
+		int playerNumber = Convert.ToInt32(tempPlayerString);
+
+		Transform newPlayerTransform = (Transform)Network.Instantiate(
+			playerPrefab, transform.position, transform.rotation, playerNumber);
+		playerScripts.Add(newPlayerTransform.GetComponent("cubeMoveAuthoritative"));
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
