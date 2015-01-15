772ec063bb7fc521a0d517148858a43118b20a60
yuvaldlv <yuvaldlv@gmail.com>
2015-01-14 18:28:41 -0800

    commit 2

---
+++ Others/UIs/Bars/Health+Mana+Stemina/Health Drainage.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthDrainage : MonoBehaviour {
+
+	public RectTransform healthDrain;
+	private float cachedY;
+	public float maxXValue;
+	public float minXValue;
+	private int currentHealth;
+	public int maxHealth;
+	public Text healthText;
+	public Image visualHealth;
+
+	// Use this for initialization
+	void Start () {
+
+		cachedY = healthDrain.position.y;
+		maxXValue = healthDrain.position.x;
+		minXValue = healthDrain.position.x - healthDrain.rect.width;
+		currentHealth = maxHealth;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void handleHealth()
+	{
+		
+		healthText.text = "Current Health: " + currentHealth;
+
+//		float currentXValue = MapValues(currentHealth, 0, maxHealth, minXValue, maxXValue);
+
+		healthDrain.position = new Vector3 (currentXValue, cachedY);
+	}
+
+	private void MapValues(float x, float inMin, float inMax, float outMin, float outMax){
+	
+		return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	
+	}
+
+}
