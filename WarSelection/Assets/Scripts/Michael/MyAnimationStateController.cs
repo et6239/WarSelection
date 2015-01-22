b018127853ac540cd2f62bcb6557a217cbf5539a
Michael Vaganov <mvaganov@hotmail.com>
2015-01-21 18:57:19 -0800

    pause UI
    * pause menu goes out at first
    * possibility for reticle to disappear when UI changes

--- WarSelection/Assets/Scripts/Michael/MyAnimationStateController.cs
+++ WarSelection/Assets/Scripts/Michael/MyAnimationStateController.cs
@@ -1,18 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MyAnimationStateController : MonoBehaviour {
 
+	public string triggerName = "pausebutton"; 
+	public bool triggerOnStart = true;
+	public List<GameObject> thingsToToggle;
+
+	void ToggleUI() {
+		Animator a = GetComponent<Animator>();
+		a.SetTrigger(triggerName);
+		if(thingsToToggle != null) {
+			for(int i = 0; i < thingsToToggle.Count; ++i) {
+				thingsToToggle[i].SetActive( !thingsToToggle[i].activeSelf );
+			}
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		if(triggerOnStart) {
+			ToggleUI();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Escape)) {
-			Animator a = GetComponent<Animator>();
-			a.SetTrigger("pausebutton");
+			ToggleUI();
 		}
 	}
 }
