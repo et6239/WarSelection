aeb87a37a2baa7041253d6ec547ad8e6329fd1a3
yuvaldlv <yuvaldlv@gmail.com>
2015-01-21 18:55:18 -0800

    hello bullet

--- WarSelection/Assets/Scripts/General/InitialVelocity.cs
+++ WarSelection/Assets/Scripts/General/InitialVelocity.cs
@@ -1,8 +1,7 @@
 using UnityEngine;using System.Collections;
 [RequireComponent (typeof (Rigidbody))] 
 public class InitialVelocity : MonoBehaviour 
-{	public Vector3 initialVelocity; 
-	void Start ()	{		if (rigidbody.velocity == Vector3.zero) 
-		{       			rigidbody.velocity = initialVelocity;		}
+{	public float speed = 100;	public Vector3 initialVelocity;	void Start ()	{		if (rigidbody.velocity == Vector3.zero) 
+		{       			rigidbody.velocity = transform.forward * speed;		}
 	}
 }
