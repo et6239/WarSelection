5d4221318fd47715a45150252be06a5be4d12828
Michael Vaganov <mvaganov@hotmail.com>
2015-02-04 18:10:32 -0800

    commit

---
+++ WarSelection/Assets/Scripts/Michael/AttackingArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackingArea : MonoBehaviour {
+
+	public float attackCooldown = 1;
+	public float damage = 5;
+	float cooldown;
+
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update () {
+		if(cooldown > 0) {
+			cooldown -= Time.deltaTime;
+			if(cooldown < 0)
+				cooldown = 0;
+		}
+	}
+		
+	void OnTriggerStay(Collider other) {
+		if(cooldown == 0) {
+			PlayerStats ps = other.gameObject.GetComponent<PlayerStats> ();
+			if(ps != null) {
+				ps.hitpoints.adjust(-damage);
+				cooldown = attackCooldown;
+			}
+		}
+	}
+}
---
+++ WarSelection/Assets/Scripts/Michael/DamageArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageArea : MonoBehaviour {
+
+	public float damagePerSecond = 2;
+
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update () { }
+
+	void OnTriggerStay(Collider other) {
+		PlayerStats ps = other.gameObject.GetComponent<PlayerStats> ();
+		if(ps != null) {
+			ps.hitpoints.adjust(-damagePerSecond * Time.deltaTime);
+		}
+	}
+}
